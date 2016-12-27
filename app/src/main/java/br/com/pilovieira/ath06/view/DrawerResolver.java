9be4ed0a662ac27b44d6378efd27a49749ed65e0
Luiz Paulo Vieira <pilovieira@gmail.com>
2016-12-27 17:47:48 -0200

    menus and links

--- app/src/main/java/br/com/pilovieira/ath06/view/DrawerResolver.java
+++ app/src/main/java/br/com/pilovieira/ath06/view/DrawerResolver.java
@@ -1,6 +1,5 @@
 package br.com.pilovieira.ath06.view;
 
-import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,14 +7,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
 
 import br.com.pilovieira.ath06.R;
-import br.com.pilovieira.cartracker.client.comm.ClientEmitters;
-import br.com.pilovieira.cartracker.client.live.LiveTrackerFragment;
-import br.com.pilovieira.cartracker.client.lock.LockFragment;
 import br.com.pilovieira.ath06.log.InfoFragment;
-import br.com.pilovieira.cartracker.client.utils.Alerts;
-import br.com.pilovieira.cartracker.client.view.OptionsActivity;
-import br.com.pilovieira.cartracker.client.view.VirtualFenceFragment;
-import br.com.pilovieira.ath06.comm.Message;
 
 public class DrawerResolver implements NavigationView.OnNavigationItemSelectedListener{
 
@@ -25,39 +17,29 @@ public class DrawerResolver implements NavigationView.OnNavigationItemSelectedLi
         this.activity = activity;
     }
 
-    public void loadInfoFragment(InfoFragment infoFragment) {
-        replaceFragment(infoFragment);
-        activity.setTitle(R.string.app_name);
-        closeDrawers();
-    }
-
     @Override
     public boolean onNavigationItemSelected(MenuItem menuItem) {
-        boolean resolved = resolveFunctions(menuItem);
+        Fragment fragment = null;
 
-        if (!resolved) {
-            Fragment fragment = null;
-
-            switch(menuItem.getItemId()) {
-                case R.id.nav_info:
-                    fragment = new InfoFragment();
-                    break;
-                case R.id.nav_lock:
-                    fragment = new LockFragment();
-                    break;
-                case R.id.nav_live:
-                    fragment = new LiveTrackerFragment();
-                    break;
-                case R.id.nav_virtual_fence:
-                    fragment = new VirtualFenceFragment();
-                    break;
-            }
-
-            replaceFragment(fragment);
-
-            menuItem.setChecked(true);
-            activity.setTitle(menuItem.getTitle());
+        switch(menuItem.getItemId()) {
+            case R.id.nav_info:
+                fragment = new InfoFragment();
+                break;
+            case R.id.nav_operations:
+                fragment = new OperationsFragment();
+                break;
+            case R.id.nav_configurations:
+                fragment = new ConfigurationsFragment();
+                break;
+            case R.id.nav_user_manual:
+                fragment = new UserManualFragment();
+                break;
         }
+
+        replaceFragment(fragment);
+
+        menuItem.setChecked(true);
+        activity.setTitle(menuItem.getTitle());
 
         closeDrawers();
         return true;
@@ -71,38 +53,6 @@ public class DrawerResolver implements NavigationView.OnNavigationItemSelectedLi
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = activity.getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.content_main_frame, fragment).commit();
-    }
-
-    private boolean resolveFunctions(MenuItem menuItem) {
-        switch(menuItem.getItemId()) {
-            case R.id.nav_get_location:
-                emitFunction(menuItem, new Localization(), R.string.function_location_message);
-                break;
-            case R.id.nav_get_wallet:
-                emitFunction(menuItem, new Wallet(), R.string.function_wallet_message);
-                break;
-            case R.id.nav_get_status:
-                emitFunction(menuItem, new Status(), R.string.function_status_message);
-                break;
-            case R.id.action_settings:
-                activity.startActivity(new Intent(activity, OptionsActivity.class));
-                break;
-            default:
-                return false;
-        }
-
-        return true;
-    }
-
-    public void emitFunction(MenuItem menuItem, Message function, int successMessage) {
-        Alerts alerts = new Alerts(activity);
-
-        try {
-            new ClientEmitters(activity).emit(function);
-            alerts.showAlert(menuItem.getTitle().toString(), activity.getString(successMessage));
-        } catch (Exception e) {
-            alerts.showErrorAlert(e.getMessage());
-        }
     }
 
 }
---
+++ app/src/main/java/br/com/pilovieira/ath06/view/OperationsFragment.java
@@ -0,0 +1,65 @@
+package br.com.pilovieira.ath06.view;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import br.com.pilovieira.ath06.R;
+import br.com.pilovieira.ath06.log.ServerLog;
+import br.com.pilovieira.ath06.log.ServerLogManager;
+
+public class OperationsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+
+        ListView logList = (ListView) view.findViewById(R.id.fragment_info_message_list);
+
+        ArrayAdapter<String> changeList = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, 0, getLogs());
+        logList.setAdapter(changeList);
+
+        Button btnClear = (Button) view.findViewById(R.id.fragment_server_log_clear);
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ServerLogManager(view.getContext()).clearLogs();
+            }
+        });
+
+        return view;
+    }
+
+    private List<String> getLogs() {
+        List<String> logsString = new ArrayList<>();
+
+        List<ServerLog> logs = new ServerLogManager(getContext()).getLogs();
+        Collections.sort(logs);
+        Collections.reverse(logs);
+
+        for (ServerLog log : logs) {
+            StringBuilder b = new StringBuilder();
+            if (log.getDate() != null)
+                b.append(new SimpleDateFormat("dd/MM/yy hh:mm:ss").format(log.getDate())).append("\n");
+            if (log.getTitle() != null && !log.getTitle().isEmpty())
+                b.append(log.getTitle()).append("\n");
+            b.append(log.getMessage()).append("\n");
+
+            logsString.add(b.toString());
+        }
+
+        return logsString;
+    }
+
+}
